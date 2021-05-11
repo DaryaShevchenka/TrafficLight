dc3b8c46dc9d5deac44b33c241acf9b7558e80e9
Dashechka-blond <d.shevchenka@gmail.com>
2021-05-11 08:44:38 +0300

    Create enum

--- TrafficLight/TrafficLight/ViewController.swift
+++ TrafficLight/TrafficLight/ViewController.swift
@@ -27,19 +27,62 @@ class ViewController: UIViewController {
         
     }
 
+    enum ColorOfLight {
 
-    @IBAction func startButton(_ sender: UIButton) {
-        redLight.alpha = 1
+    case  redLight
+    case  yellowLight
+    case  greenLight
+    case  noLight
+}
+   
+    var color = -1
+   
+    var arrayOfColor = [ColorOfLight.redLight, ColorOfLight.yellowLight, ColorOfLight.greenLight, ColorOfLight.noLight]
+
+
+    @IBAction func startButton(_ sender: UIButton){
+         color += 1
+        let currentLight = arrayOfColor[color % 4]
+        
+        switch currentLight {
+        case .redLight:
+            
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            startPressed.setTitle("NEXT", for: .normal)
+        case .yellowLight:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case .greenLight:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+        case .noLight:
+            redLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            startPressed.setTitle("START", for: .normal)
+        }
+    }
+    
+}
+      /*
+        if redLight.alpha == 1{
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
         startPressed.setTitle("NEXT", for: .normal)
-        
-      
-            
-            
-            
-            
+    
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1{
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        }else{
+            redLight.alpha = 1
         }
-        }
+    }
+}*/
+    
        
-        
-
-
